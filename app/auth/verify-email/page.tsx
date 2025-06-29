a225c3c20fbfee4d7d881550bf288efaf5159cd3
Rabiu Ahmad O. <rabiu2993@gmail.com>
2025-06-29 18:02:44 +0100

    Fix: Refacto verify email page code

--- app/auth/verify-email/page.tsx
+++ app/auth/verify-email/page.tsx
@@ -1,23 +1,6 @@
-"use client";
-
-import { Button } from "@/components/ui/button";
-import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
 import { CircleCheck } from "lucide-react";
-import Link from "next/link";
-import { FormEvent } from "react";
 
 const VerifyEmail = () => {
-  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
-    const form = e.currentTarget;
-    const formData = new FormData(form);
-
-    // const credentials = {
-    //   email: formData.get("email"),
-    //   password: formData.get("password"),
-    // };
-  };
-
   return (
     <main className="min-h-screen flex flex-col justify-center items-center w-full">
       <div className="space-y-4 w-full px-6 max-w-md py-8 rounded-lg border flex flex-col justify-center items-center">
